99f2491c34e92f14095cd3543d539669b77e6716
Yuri Dimitre Dias de Faria <yuri.faria@enacom.com.br>
2023-12-14 09:30:38 -0300

    .

--- to_excel.py
+++ to_excel.py
@@ -1,11 +1,11 @@
 import pandas as pd
-
+import sys
 # Ler o arquivo CSV
-arquivo_csv = 'output.csv'
+arquivo_csv = sys.argv[1]
 dados_csv = pd.read_csv(arquivo_csv)
-
+# print(arquivo_csv[:-4])
 # Especificar o nome do arquivo XLSX de saída
-arquivo_xlsx = 'saida.xlsx'
+arquivo_xlsx = f'{arquivo_csv[:-4]}.xlsx'
 
 # Salvar os dados no arquivo XLSX
 dados_csv.to_excel(arquivo_xlsx, index=False)
